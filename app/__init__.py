752d86c91cdc8a9697b0ae060c880021d265c067
HectorGit <hectorandres.pv@gmail.com>
2021-01-27 14:36:34 -0800

    how do we add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask #, flash, redirect, url_for, session, request
 from config import Config
 
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 app.config.from_object(Config) #I think this should work ? 
+bootstrap = Bootstrap(app)
 
 # app.jinja_env.globals.update(API_ROUTE=app.config['API_ROUTE'])
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
