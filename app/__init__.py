c3a73e67356b1b16d9df2ee416960bcfc6cd5d56
HectorGit <hectorandres.pv@gmail.com>
2021-01-27 13:33:29 -0800

    getting ready to deploy to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask #, flash, redirect, url_for, session, request
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config) #I think this should work ? 
 
 # app.jinja_env.globals.update(API_ROUTE=app.config['API_ROUTE'])
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
