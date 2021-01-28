8dd83a9dfcd297adb6991e5d0fec520b3b9cc630
HectorGit <hectorandres.pv@gmail.com>
2021-01-28 10:53:39 -0800

    added routes that communicate with the api

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from app import app #means to get the app flask intance from the /app package
-from flask import render_template #what other flask things do we need ?
-
+#from flask import render_template, jsonify #what other flask things do we need ?
+from flask import render_template, flash, redirect, url_for, request, jsonify, \
+make_response, session, Response
+import requests
+import json #why is this not in the requirements.txt file ???
 #how do we get Jinja2?
+
+# ------------------------------- GLOBAL VARIABLES --------------------------
+
+API_URL = app.config['API_ROUTE']
+
+# ------------------------------- GLOBAL VARIABLES --------------------------
+
 
 @app.route('/')
 def index():
@@ -11,3 +21,40 @@ def index():
 def simple_test():
     users = [{'username':'Hector'}, {'username':'Sergio'}]
     return render_template('index.html', users=users, title='Home')
+
+@app.route('/friend')
+def friend():
+    #in the flask app we have an api_handlers.py file, where this is streamlined, but at this moment i just want to try if i can make it work just like this ... 
+    url = API_URL + '/read_friend' #not sure if this is the route that actually exists ? 
+    # we can make another endpoint in the api 
+    data = {
+        'first_name': 'jordie'
+    }
+    r = requests.get(url, data=data)
+    friend = r.json() #not sure if this would work
+    if r.status_code == 200:
+        #flash(r.json()['message'], 'success')
+        print('was able to retrieve friend from API')
+        print(friend) #expect to print form in json ...
+    else:
+        #flash(r.json()['message'], 'danger')
+        print('unable to retrieve friend from API')
+        print('r: ', r)
+    return render_template('friend.html', friend = friend)
+
+@app.route('/friends')
+def friends():
+    #in the flask app we have an api_handlers.py file, where this is streamlined, but at this moment i just want to try if i can make it work just like this ... 
+    url = API_URL + '/read_all_friends' #not sure if this is the route that actually exists ? 
+    # we can make another endpoint in the api 
+    r = requests.get(url)
+    friends = r.json() #not sure if this would work
+    if r.status_code == 200:
+        #flash(r.json()['message'], 'success')
+        print('was able to retrieve friend from API')
+        print(friends) #expect to print form in json ...
+    else:
+        #flash(r.json()['message'], 'danger')
+        print('unable to retrieve friend from API')
+        print('r: ', r)
+    return render_template('friends.html', friends = friends)
--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 class Config(object):
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:postgres@localhost:5432/h_practice' 
-    API_ROUTE = os.environ.get('API_ROUTE') or 'http://localhost:5001'
+    API_ROUTE = os.environ.get('API_ROUTE') or 'http://localhost:5001' #make sure hector-practice-api is running on localhost:5001
     FLASK_CONFIG =  os.environ.get('FLASK_CONFIG') or 'localhost'
     # LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
