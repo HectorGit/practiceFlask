07526f07e1e6e72c3dab34315f5d41e8f5d6cbb9
HectorGit <hectorandres.pv@gmail.com>
2021-01-29 16:21:50 -0800

    change to see if I could do some linking with other files via assets , will ask

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -5,3 +5,23 @@ $(document).ready(function () {
     console.log('document ready , javascript connected')
 
 });
+
+$("#mc-embedded-subscribe-form").on("submit", function (event) {  
+    console.log('clicked subscribe button') 
+    event.preventDefault();
+//     var form = document.getElementById('mc-embedded-subscribe-form')
+//     $.ajax({
+//         type: 'POST',
+//         url: "/add_newsletter",
+//         data: {
+//             "email_address": form.EMAIL.value
+//         },
+//         success: function (response) {
+//             return console.log("success!")
+//         },
+//         error: function (error) {
+//             console.log("###################")
+//             return console.log(error)
+//         }
+//     })
+})
