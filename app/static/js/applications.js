4e23bb0eda81bb7a73ed41d18ff948e0626664a3
HectorGit <hectorandres.pv@gmail.com>
2021-02-08 15:57:49 -0800

    all pages except join and blog

--- app/static/js/applications.js
+++ app/static/js/applications.js
@@ -9,4 +9,27 @@ $(document).ready(function () {
 
     current_tab_hyperlink.addClass('current')
 
+
+    var applications_slider = tns({
+        container:'#applications_slider',
+        items: 1
+        ,
+        controls: false,
+        nav: false 
+    })
+
+    
+    $('#pv_blinds_button').click(function () {
+        applications_slider.goTo(0)
+    })
+
+    $('#pv_tiles_button').click (function () {
+        applications_slider.goTo(1)
+    })
+
+
+    $('#pv_windows_button').click (function () {
+        applications_slider.goTo(2)
+    })
+
 });
